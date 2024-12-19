12f0290d13449ae9f7b8f00ec9b628dd3e2f4042
Benjamin Kan <Kan@ibm.com>
2024-12-19 13:51:08 -0800

    Initial gradle multi project setup with an empty REST service

---
+++ system-service/src/main/java/de/b3nk4n/system/SystemApplication.java
@@ -0,0 +1,8 @@
+package de.b3nk4n.system;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class SystemApplication extends Application {
+}
